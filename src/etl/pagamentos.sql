90bf05f16dc314039bb414ea7e18f580a68c6c74
Matheus Magalhães <matheus.magalha95@gmail.com>
2023-04-06 17:44:40 -0300

    ETL de pagamentos Completo

--- src/etl/pagamentos.sql
+++ src/etl/pagamentos.sql
@@ -36,36 +36,51 @@ tb_group AS (
     FROM tb_join
     GROUP BY idVendedor, descTipoPagamento
     ORDER BY idVendedor, descTipoPagamento
-)
+),
 
 -- [ x ] exibindo os valores de cada vendeodr em uma única linha (qtdePagamentos, valorTOTPagamento, ProporçãoPedidos, PorcentagemPagamentoVendedor)
-SELECT
-    idVendedor,
 
-    -- quantidade total de cada tipo de pagamento pra cada vendedor
-    sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) as qtde_boleto_pedido,
-    sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_credit_card_pedido,
-    sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) as qtde_voucher_pedido,
-    sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_debit_card_pedido,
+tb_summary AS (
+    SELECT
+        idVendedor,
 
-    -- receita total de cada meio de pagamento para cada vendedor
-    sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) as valor_boleto_pedido,
-    sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) as valor_credit_card_pedido,
-    sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) as valor_voucher_pedido,
-    sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) as valor_debit_card_pedido,
+        -- quantidade total de cada tipo de pagamento pra cada vendedor
+        sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) as qtde_boleto_pedido,
+        sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_credit_card_pedido,
+        sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) as qtde_voucher_pedido,
+        sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) as qtde_debit_card_pedido,
 
-    -- porcentagem de pedidos pagos por boleto em relação ao total de pedidos realizados. Se a proporção resultar em 0, isso indica que nenhum pedido foi pago por boleto, enquanto uma proporção de 1 indica que todos os pedidos foram pagos por boleto.
-    sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_boleto_pedido,
-    sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_credit_card_pedido,
-    sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_voucher_pedido,
-    sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_debit_card_pedido,
+        -- receita total de cada meio de pagamento para cada vendedor
+        sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) as valor_boleto_pedido,
+        sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) as valor_credit_card_pedido,
+        sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) as valor_voucher_pedido,
+        sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) as valor_debit_card_pedido,
 
-    -- para descobrir o percental, basta dividir o TOTAL DE CADA MEIO DE PAGAMENTO desse vendedor pela RECEITA TOTAL de todos os meios
-    sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_boleto_pedido,
-    sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_credit_card_pedido,
-    sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_voucher_pedido,
-    sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
+        -- porcentagem de pedidos pagos por boleto em relação ao total de pedidos realizados. Se a proporção resultar em 0, isso indica que nenhum pedido foi pago por boleto, enquanto uma proporção de 1 indica que todos os pedidos foram pagos por boleto.
+        sum(case when descTipoPagamento='boleto' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_boleto_pedido,
+        sum(case when descTipoPagamento='credit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_credit_card_pedido,
+        sum(case when descTipoPagamento='voucher' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_voucher_pedido,
+        sum(case when descTipoPagamento='debit_card' then qtdePedidoMeioPagamento else 0 end) / sum(qtdePedidoMeioPagamento) as pct_qtd_debit_card_pedido,
 
-FROM tb_group
+        -- para descobrir o percental, basta dividir o TOTAL DE CADA MEIO DE PAGAMENTO desse vendedor pela RECEITA TOTAL de todos os meios
+        sum(case when descTipoPagamento='boleto' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_boleto_pedido,
+        sum(case when descTipoPagamento='credit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_credit_card_pedido,
+        sum(case when descTipoPagamento='voucher' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_voucher_pedido,
+        sum(case when descTipoPagamento='debit_card' then vlPedidoMeioPagamento else 0 end) / sum(vlPedidoMeioPagamento) as pct_valor_debit_card_pedido
 
-GROUP BY idVendedor
+    FROM tb_group
+
+    GROUP BY idVendedor
+)
+
+
+-- SELECT idVendedor,
+--        AVG(nrParcelas) AS avgQtdeParcelas
+
+-- FROM tb_join
+
+-- WHERE descTipoPagamento = 'credit_card'
+
+-- GROUP idVendedor
+
+    --    (SELECT nrParcelas FROM tb_join ORDER BY nrParcelas LIMIT 1 OFFSET (SELECT COUNT(*) FROM tb_join) / 2)
